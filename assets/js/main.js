443fe4c13a11b0619bded8d449e0ee21a0fe5620
sopiadi19 <sopiadi2006@gmail.com>
2024-02-18 23:58:59 +0700

    FINSIH

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,26 +9,27 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 const scrollUp = () => {
-    const scrollUp = document.getElementById('scrollup')
+    const scrollUp = document.getElementById('scroll-up')
     //when the scroll is higher than 350 viewport height, add the show-scroll class to header
-    this.scrollY >= 50 ? header.classList.add('show-scroll')
-                       : header.classList.remove('show-scroll')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
 }
-window.addEventListener('scroll', scrollup)
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () => {
+const scrollActive = () =>{
     const scrollY = window.pageYOffset
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
                 sectionTop = current.offsetTop - 58,
                 sectionId = current.getAttribute('id'),
-                sectionsClass = document.querySelectorAll('.nav_menu a[href*='+ sectionId +']')
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                sectionsClass = document.querySelector('.nav_menu a[href*='+ sectionId +']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             sectionsClass.classList.add('active-link')
         }else{
             sectionsClass.classList.remove('active-link')
@@ -46,11 +47,11 @@ const sr = ScrollReveal({
     //reset true // animation repeat
 })
 
-sr.reveal('.home_data, .products_data .steps_content footer_container')
+sr.reveal('.home_data, .products_data, .steps_content, footer_container')
 sr.reveal('.home_img', {origin: 'bottom'})
 sr.reveal('.products_card', {inteval: 100})
-sr.reveal('.about_img .testimonial_img', {origin: 'right'})
-sr.reveal('.about_img .testimonial_img', {origin: 'left'})
+sr.reveal('.about_img, .testimonial_img', {origin: 'right'})
+sr.reveal('.about_img, .testimonial_img', {origin: 'left'})
 
 
 
